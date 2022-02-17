8a756bf6f698a9a7fe0a90912671d4ad34d82652
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-02-17 11:16:21 -0700

    Initial Commit

---
+++ H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by Mitch Andrade on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
